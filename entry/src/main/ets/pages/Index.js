7fc82364b25aad100edbf37617f73d49ad96bd2d
zhangtian <zhangtian@weidian.com>
2024-03-31 18:40:15 +0800

    feat: init project

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,40 @@
+struct Index extends   {
+    constructor() { }
+    changeMsg(newVal) {
+        // this.message = newVal
+    }
+    build() {
+        ;
+    }
+}
+struct OrderItem extends   {
+    constructor() { }
+    build() {
+    }
+}
+struct FilterBar extends   {
+    constructor() { }
+    build() {
+        ;
+    }
+}
+function fancyText({ size, color }) {
+    
+  
+        .fontSize(size)
+        .fontColor(color);
+}
+struct NavBar extends   {
+    constructor() { }
+    build() {
+        .justifyContent(FlexAlign.SpaceBetween).width("100%");
+    }
+}
+struct SearchBar extends   {
+    constructor() { }
+    build() {
+        ;
+    }
+}
+export {};
+//# sourceMappingURL=Index.js.map
